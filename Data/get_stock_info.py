64c33640868a7d94494aecf4d847b66302ed1e35
Ryan <22962262+MetalDrgn@users.noreply.github.com>
2023-12-02 10:29:42 -0800

    improved and adding sorting for get stocks.

--- Data/get_stock_info.py
+++ Data/get_stock_info.py
@@ -1,5 +1,5 @@
-""" Retrieves stock information (like if it's fractional) for quick reference in repository.
-Entered individually in the terminal or as a list (['ex1', 'ex2']).
+""" Retrieves stock information (like if it accepts fractional trades) for quick reference and stores it in stocks.json.
+Entered as an arguement individually in the terminal or as a list (['ex1', 'ex2']).
 Can be entered as argument.
 Will automatically run and add if stock not in stocks.py. """
 import requests
@@ -30,19 +30,24 @@ with open(filename, "r") as f:
 
 
 class StockUpdater:
-    def __init__(self, stocks=[]):
-        self.stocks = stocks
+# TODO: Add add and remove options. Code implemented but currently only add is setup for arguments. (--add/--remove)
+# TODO: Add options for changing stock buy preferences (--account real ['MSFT', 'GOOG'], --account None MSFT).
+    def __init__(self, stocklist=[]):
+        self.stocklist = stocks
+
+    def sort(self):
+        # self.stocklist = sorted(self.stocklist, key=lambda stock: stock['symbol'])
+        list.sort(self.stocklist, key=lambda stock: stock['symbol'])
 
     def stockSplitter(self, assetList):
-        # newStocks = []
+        # For adding new stocks. Determines if the input is an indicidual stock or list.
         if isinstance(assetList, list):
-            # newStocks = self.Multistock(assetList)
             self.Multistock(assetList)
         elif isinstance(assetList, str):
-            # newStocks.append(self.getStockInfo(assetList))
             self.updateStockInfo(self.getStockInfo(assetList))
         else:
             raise Exception(f"stockSplitter received wrong type {type(assetList)}")
+        sorted(self.stocklist, key=lambda stock: stock['symbol'])
         self.writeStockInfo()
 
     def Multistock(self, assetList):
@@ -55,8 +60,6 @@ class StockUpdater:
 
     def getStockInfo(self, asset):
         # Get individual stock information and store it in stocks.JSON in the Data directory.
-        # asset = "CLSK".upper()
-        # asset = "CLSK".upper()
         account = getKeys("paperTrading")
         url = "https://paper-api.alpaca.markets/v2/assets/" + asset.upper()
 
@@ -71,15 +74,15 @@ class StockUpdater:
 
     def updateStockInfo(self, asset):
         # Adds or updates one asset. Adds account key to asset dictionary for user account preference ("paper", "real"). Also updates the asset data if there are changes.
-        for n, stock in enumerate(self.stocks):
+        for n, stock in enumerate(self.stocklist):
             if asset["symbol"] == stock["symbol"]:
                 if not "account" in stock:
-                    self.stocks[n]["account"] = ""
-                self.stocks[n].update(asset)
+                    self.stocklist[n]["account"] = ""
+                self.stocklist[n].update(asset)
                 break
         else:
             asset["account"] = ""
-            self.stocks.append(asset)
+            self.stocklist.append(asset)
         return
 
     def stockRemover(self, asset):
@@ -94,26 +97,27 @@ class StockUpdater:
 
     def removeStock(self, asset):
         # removes individual stocks (ex. "AAPL")
-        for n, stock in enumerate(self.stocks):
+        for n, stock in enumerate(self.stocklist):
             if asset.upper() == stock["symbol"]:
-                self.stocks.pop(n)
+                self.stocklist.pop(n)
                 break
         else:
-            print(f"Stock not found: {asset}")
+            print(f"Stock not found in stocks list to remove: {asset}")
 
     def writeStockInfo(self):
+        self.sort()
         with open(filename, "w+") as f:
-            # f.write(stocks)
-            json.dump(self.stocks, f, indent=4)
+            json.dump(self.stocklist, f, indent=4)
             
     def getAccountPreference(self):
-        if self.stocks == []:
+        if self.stocklist == []:
             print('Empty stocks list')
             return
+        self.sort()
         real = []
         paper = []
         neither = []
-        for stock in self.stocks:
+        for stock in self.stocklist:
             if stock['account'] == '':
                 neither.append(stock['symbol'])
             elif stock['account'].upper() == 'real'.upper():
@@ -124,7 +128,7 @@ class StockUpdater:
 
 
 if __name__ == "__main__":
-    # TODO: Add add and remove options. Code implemented but currently only add is setup for arguments.
+
     try:
         print(f"Adding: {sys.argv[1]}")
         try:
@@ -135,10 +139,8 @@ if __name__ == "__main__":
         manualStockAdd.stockSplitter(inputList)
     except IndexError:
         clstest = StockUpdater(stocks)
+        # clstest.stockRemover('pltr')
         clstest.getAccountPreference()
-        # None
-        # print('no arg provided')
         # clstest.stockSplitter("fcel")
-        # clstest.stockRemover('goog')
         # clstest.stockRemover(["NVDA","MSFT"])
         # clstest.stockSplitter(["NVDA","MSFT"])
