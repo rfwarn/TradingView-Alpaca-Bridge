6deec44124d0af07b3b308327e8852ae12bf2cd4
Ryan <22962262+MetalDrgn@users.noreply.github.com>
2023-12-26 16:17:43 -0800

    small add

--- test_small_open_hours.py
+++ test_small_open_hours.py
@@ -1,1 +1,4 @@
 # Will be primarily used to test using paper account during normal hours. Using for validation that won't work outside trading hours.
+
+# Set this to stock that can be traded on your paper account during trading hours.
+sacrificialStock = 'JPM'
